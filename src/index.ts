2b383ced26c568a20657ea0d5d32949916297d33
zezhen.zhang <zezhen.zhang@weimob.com>
2023-07-17 17:27:11 +0800

    feat: 修改类型

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
-let a = 123;
-console.log(a);
+import axios from "axios";
+interface IPerson {
+  name: "xx";
+  age: 18;
+}
+// type Test = keyof IPerson;
+type IPartial = Partial<IPerson>;
+// type IOmit = Omit<IPerson, "name">;
+type IOmit = Omit<IPerson, "name">;
 
-let name: string = "df";
-class Dog {}
-let duoduo: Dog = new Dog();
-let arr2: (string | number)[] = [1, 2, 3, "a", "b"];
-let arr3: [string] = ["a"];
+type t0 = ReturnType<() => number>;
+type t1 = ReturnType<(s: string) => void>;
+
+interface API {
+  "/book/detail": {
+    id: number;
+  };
+  "/book/comment": {
+    id: number;
+    comment: string;
+  };
+}
+
+function request<T extends keyof API>(url: T, obj: API[T]) {
+  return axios.post(url, obj);
+}
+
+request("/book/comment", {
+  id: 1,
+  comment: "aaa",
+});
+
 export {};
